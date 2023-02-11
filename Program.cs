9bc1acb2577dda23861dfe2e76ad4a8b9a6ff6b4
Irina632015 <irina.632015@yandex.ru>
2023-02-11 22:57:25 +0400

    Семинар 9 Задача 66

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+System.Console.WriteLine(SumInterval(4, 8));
+
+int SumInterval(int start, int end)
+{
+    if(start == end)
+    {
+        return start;
+    }
+    return start + (SumInterval(start + 1, end));
+}
